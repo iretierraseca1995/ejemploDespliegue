8f41e8af00433aff593784b1001bc873f39253ec
Irene <Irene@DESKTOP-ONSBGUJ>
2019-03-10 17:11:17 +0100

    miniCalculadora

---
+++ src/java/miniCalculadoraEjemplo.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Irene
+ */
+public class miniCalculadoraEjemplo {
+    
+    /**
+     * Calcula el valor asbsoluto de un número
+     * 
+     * @param numero double
+     * @return double 
+     */
+    public double valorAbsoluto(double numero){
+        double x = Math.abs(numero);
+        return x;
+    }
+    
+    /**
+     * Calcula la raiz cuadrada de un número
+     * 
+     * @param numero double
+     * @return double
+     */
+    public double raizCuadrada(double numero){
+        double x = Math.sqrt(numero);
+        return x;
+    }
+}
